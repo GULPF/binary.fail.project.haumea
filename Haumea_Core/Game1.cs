e5d73f0fdfad8eff12efbdf582364b5cfa52c0c3
GULPF <oscarnihlgard@gmail.com>
2016-02-09 21:00:18 +0000

    Moved extensions out of main class

--- Haumea_Core/Game1.cs
+++ Haumea_Core/Game1.cs
@@ -169,13 +169,5 @@ namespace Haumea_Core
             return v;
         }
     }
-
-    public static class PointExt
-    {
-        public static Vector2 ToVector2(this Point point)
-        {
-            return new Vector2(point.X, point.Y);
-        }
-    }
 }
 
---
+++ Haumea_Core/VectorExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Haumea_Core
+{
+    public static class VectorExtensions
+    {
+        public static Vector2 ToVector2(this Point point)
+        {
+            return new Vector2(point.X, point.Y);
+        }
+
+        public static Vector2 ToVector2(this Vector3 v3d)
+        {
+            return new Vector2(v3d.X, v3d.Y);
+        }
+
+        public static Vector3 ToVector3(this Vector2 v2d)
+        {
+            return new Vector3(v2d.X, v2d.Y, 0);
+        }
+    }
+}
+
