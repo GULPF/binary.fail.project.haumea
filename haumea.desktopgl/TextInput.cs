0bfdf4171e9f0ee6fa8cd4679da6f2ddc415ba9b
GULPF <oscarnihlgard@gmail.com>
2016-09-18 09:31:01 +0200

    Initial work on text input dialogs

--- haumea.desktopgl/TextInput.cs
+++ haumea.desktopgl/TextInput.cs
@@ -1,11 +1,27 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
 
 namespace haumea.desktopgl
 {
+    public interface ITextInput
+    {
+        event Action<TextInputEventArgs> TEvent;
+    }
+
     public class TextInput
     {
-        public TextInput()
+        event Action<TextInputEventArgs> TEvent;
+
+        public TextInput(GameWindow window)
         {
+            window.TextInput += (sender, e) => 
+            {
+                var handle = TEvent;
+                if (handle != null)
+                {
+                    handle(e);
+                }
+            };
         }
     }
 }
